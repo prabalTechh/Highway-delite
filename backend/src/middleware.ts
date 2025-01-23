77f50df1d9b1046d9493264d9bfa934d30232a0d
Prabal Chanpuria <id.prabal@gmail.com>
2025-01-23 15:45:22 +0530

    added middleware and added notes schema

---
+++ backend/src/middleware.ts
@@ -0,0 +1,22 @@
+import jwt, { JwtPayload } from "jsonwebtoken"
+import { NextFunction, Request, Response } from "express";
+
+export const middleware = (req:Request,res:Response,next:NextFunction) =>{
+
+   const token = req.headers["authorization"] || "";
+
+   const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as JwtPayload;
+
+   if(decoded){
+
+    //@ts-ignore
+    req.userId = decoded.userId;
+    next();
+   }else{
+    res.status(402).json({
+        message : "unauthorized!!"
+    })
+   }
+
+    
+}
